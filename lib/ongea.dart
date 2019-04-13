ddf3fb10a8f72791398358b9d3568fd4c9217bc4
mj <onyangomaureen95@gmail.com>
2019-04-13 22:57:39 +0300

    place textfield with a push button

--- lib/ongea.dart
+++ lib/ongea.dart
@@ -1,8 +1,42 @@
 import 'package:flutter/material.dart';
 
+
 class Ongea extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return Center(child: Text('Ongea page'));
+  Widget build(context) {
+    return Scaffold(
+      resizeToAvoidBottomPadding: false,
+      body: Container(
+        padding: EdgeInsets.only(top: 50.0),
+        child: Form(
+          child: username(),
+        ),
+      ),
+    );
+  }
+
+  Widget username() {
+    return Column(children: [
+      Text(
+        'Provide a username',
+        style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.w700),
+      ),
+      SizedBox(
+        height: 10.0,
+      ),
+      TextFormField(
+        keyboardType: TextInputType.text,
+        decoration: InputDecoration(
+          labelText: 'username',
+          hintText: 'Unique name',
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(30.0)),
+        ),
+        onSaved: (String name) {
+          name = name;
+        },
+      ),
+      FlatButton.icon(
+          onPressed: () {}, icon: Icon(Icons.check), label: Text('Proceed'))
+    ]);
   }
 }
