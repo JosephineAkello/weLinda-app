374063c7282226ae8f92f0cb1ac26b6124bce816
mj <onyangomaureen95@gmail.com>
2019-04-14 00:22:29 +0300

    show alert dialog for the buttons

--- lib/arifu.dart
+++ lib/arifu.dart
@@ -25,46 +25,126 @@ class Arifu extends StatelessWidget {
                   ), 
                   Card(
                     child: ListTile(
-                    title: Text('Counsellor one'),
-                    trailing: RaisedButton(
+                    title: Text('Counsellor 1'),
+                    trailing: SizedBox(
+                      child:RaisedButton(
                       child: Text('Read more..', style: TextStyle(color: Colors.white),),
                       color: Colors.purple,
                       onPressed: (){
-
+                      showDialog(
+                        context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              backgroundColor: Colors.purpleAccent,
+                              shape: BeveledRectangleBorder(),
+                              title: Text('Cousellor name'),
+                              content: Text('phonenumber'),
+                              actions: <Widget>[
+                                MaterialButton(
+                                  onPressed: (){
+                              Navigator.pop(context);
+                                  },
+                                  child: Text('close',style: TextStyle(color: Colors.purple,fontSize: 15),),
+                                ),
+                              ],
+                            );
+                          }
+                      );
                       },
+                    ),
                     ),
                     ),
                   ),
                   Card(
                     child: ListTile(
-                    title: Text('Counsellor two'),
-                    trailing: RaisedButton(
+                    title: Text('Counsellor 2'),
+                    trailing: SizedBox(
+                      child:RaisedButton(
                       child: Text('Read more..',style: TextStyle(color: Colors.white),),
                       color: Colors.purple,
                       onPressed: (){
-
+                      showDialog(
+                        context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              backgroundColor: Colors.purpleAccent,
+                              shape: BeveledRectangleBorder(),
+                              title: Text('Cousellor name'),
+                              content: Text('phonenumber'),
+                              actions: <Widget>[
+                                MaterialButton(
+                                  onPressed: (){
+                              Navigator.pop(context);
+                                  },
+                                  child: Text('close',style: TextStyle(color: Colors.purple,fontSize: 15),),
+                                ),
+                              ],
+                            );
+                          }
+                      );
                       },
+                  ),
                   ),),
                   ),
                  Card(
                     child: ListTile(
-                    title: Text('Counsellor three'),
-                    trailing: RaisedButton(
+                    title: Text('Counsellor 3'),
+                    trailing: SizedBox(
+                      child:RaisedButton(
                       child: Text('Read more..',style: TextStyle(color: Colors.white),),
                       color: Colors.purple,
                       onPressed: (){
-
+                        showDialog(
+                        context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              backgroundColor: Colors.purpleAccent,
+                              shape: BeveledRectangleBorder(),
+                              title: Text('Cousellor name'),
+                              content: Text('phonenumber'),
+                              actions: <Widget>[
+                                MaterialButton(
+                                  onPressed: (){
+                              Navigator.pop(context);
+                                  },
+                                  child: Text('close',style: TextStyle(color: Colors.purple,fontSize: 15),),
+                                ),
+                              ],
+                            );
+                          }
+                      );
                       },
                   ),),),
+                  ),
                  Card(
                     child: ListTile(
-                    title: Text('Counsellor four'),
-                     trailing: RaisedButton(
+                    title: Text('Counsellor 4'),
+                     trailing: SizedBox(
+                      child:RaisedButton(
                        child: Text('Read more..', style: TextStyle(color: Colors.white),),
                       color: Colors.purple,
                       onPressed: (){
-
+                     showDialog(
+                        context: context,
+                          builder: (context) {
+                            return AlertDialog(
+                              backgroundColor: Colors.purpleAccent,
+                              shape: BeveledRectangleBorder(),
+                              title: Text('Cousellor name'),
+                              content: Text('phonenumber'),
+                              actions: <Widget>[
+                                MaterialButton(
+                                  onPressed: (){
+                              Navigator.pop(context);
+                                  },
+                                  child: Text('close',style: TextStyle(color: Colors.purple,fontSize: 15),),
+                                ),
+                              ],
+                            );
+                          }
+                      );
                       },
+                  ),
                   ),
                   ),
                   ),
