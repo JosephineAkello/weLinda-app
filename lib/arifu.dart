71b3f80c9e35ad3a1d9c36d4279f69a1610544c2
keronei <keronei95@gmail.com>
2019-04-13 23:10:42 +0300

    Added arifu page

--- lib/arifu.dart
+++ lib/arifu.dart
@@ -1,9 +1,58 @@
 import 'package:flutter/material.dart';
 
-class Arifu extends StatelessWidget{
-  Widget build(context){
-     Widget build(BuildContext context) {
-    return Center(child: Text('Arifu page'));
+class Arifu extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(children: [
+        Column(
+          children: <Widget>[
+            Card(
+                child: Container(
+                  padding: EdgeInsets.only(left: 16.0, right: 16.0),
+                  width: MediaQuery.of(context).orientation == Orientation.portrait
+                      ? 500
+                      : 900,
+                  height: 250.0,
+                  child: Text(
+                    'Counsellors',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                )),
+            Card(
+                child: Container(
+                  padding: EdgeInsets.only(left: 16.0, right: 16.0),
+                  width: MediaQuery.of(context).orientation == Orientation.portrait
+                      ? 500
+                      : 900,
+                  height: 250.0,
+                  child: Text(
+                    'Quick reach',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                )),
+            Card(
+                child: Container(
+                  padding: EdgeInsets.only(left: 16.0, right: 16.0),
+                  width: MediaQuery.of(context).orientation == Orientation.portrait
+                      ? 500
+                      : 900,
+                  height: 250.0,
+                  child: Text(
+                    'FAQs',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                ))
+          ],
+        )
+      ]),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.phone_in_talk),
+      ),
+    );
   }
-}
 }
