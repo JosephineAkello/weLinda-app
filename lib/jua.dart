cc7565ddfd3bf51c734c4fc1c32001a96be64b69
keronei <keronei95@gmail.com>
2019-04-13 23:18:32 +0300

    Added jua page content and bit of styling

--- lib/jua.dart
+++ lib/jua.dart
@@ -1,39 +1,45 @@
 import 'package:flutter/material.dart';
 
-class Jua extends StatelessWidget{
+class Jua extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return ListView(
-     children: [
-      Card(
-       
-       child: Container(
-         padding: EdgeInsets.only(left: 16.0,),
-         height: 250.0,
-       child:Text('Information')
-       
-     ),
-    ),
-     Card(
-        child: Container(
-           padding: EdgeInsets.only(left: 16.0,),
-         height: 250.0,
-       child: Text("There's hope")
-     
-     ),
-     ),
-    Card(
-      child: Container(
-         padding: EdgeInsets.only(left: 16.0,),
-         height: 250.0,
-       child: Text('Sexual Assault')
-     ),
-    ),
-     ],
-
-    
+      children: [
+        Card(
+          child: Container(
+              padding: EdgeInsets.only(
+                left: 16.0,
+              ),
+              height: 250.0,
+              child: Text(
+                'Information',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18.0),
+              )),
+        ),
+        Card(
+          child: Container(
+              padding: EdgeInsets.only(
+                left: 16.0,
+              ),
+              height: 250.0,
+              child: Text("There's hope",
+                  textAlign: TextAlign.center,
+                  style:
+                      TextStyle(fontWeight: FontWeight.w700, fontSize: 18.0))),
+        ),
+        Card(
+          child: Container(
+              padding: EdgeInsets.only(
+                left: 16.0,
+              ),
+              height: 250.0,
+              child: Text('Sexual Assault',
+                  textAlign: TextAlign.center,
+                  style:
+                      TextStyle(fontWeight: FontWeight.w700, fontSize: 18.0))),
+        ),
+      ],
     );
   }
-
 }
