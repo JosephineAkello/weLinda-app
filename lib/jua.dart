a6baadb6136a758b6b20b2f2693951b70024fca1
mj <onyangomaureen95@gmail.com>
2019-04-13 22:44:17 +0300

    create jua file

---
+++ lib/jua.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Jua extends StatelessWidget{
+  Widget build(context){
+     Widget build(BuildContext context) {
+    return Center(child: Text('Jua page'));
+  }
+}
+}
