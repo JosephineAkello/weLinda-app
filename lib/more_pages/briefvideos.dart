9e88537baf66c66a7b05a5bd3589e13ff26db770
mj <onyangomaureen95@gmail.com>
2019-04-14 08:30:48 +0300

    change to stateful widget

--- lib/more_pages/briefvideos.dart
+++ lib/more_pages/briefvideos.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
-class BriefVideos extends StatelessWidget{
-  
+class BriefVideos extends StatefulWidget{
+  createState(){
+    return BriefVideosState();
+  }
+}
+  class BriefVideosState extends State<BriefVideos>{
+    
   Widget build(BuildContext context) {
     return Container(
-      
+
     );
   }
 }
