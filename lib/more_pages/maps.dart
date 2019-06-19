cc82ff73f9096e97d657ec824db119a1e1acb724
mj <onyangomaureen95@gmail.com>
2019-06-19 15:07:45 +0300

    place a google map controller

--- lib/more_pages/maps.dart
+++ lib/more_pages/maps.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class SearchPlace extends StatefulWidget {
   createState() {
@@ -7,7 +10,22 @@ class SearchPlace extends StatefulWidget {
 }
 
 class SearchPlaceState extends State<SearchPlace> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _position =
+      CameraPosition(target: LatLng(-0.091702, 34.767956), zoom: 14.0);
+
   Widget build(context) {
-    return Scaffold();
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Search Recovery Center'),
+          backgroundColor: Colors.purple,
+        ),
+        body: GoogleMap(
+          initialCameraPosition: _position,
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+        ));
   }
 }
