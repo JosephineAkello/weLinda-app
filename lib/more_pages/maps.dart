866c1e01145b5dc245920daed274813049589d96
mj <onyangomaureen95@gmail.com>
2019-06-19 09:49:25 +0300

    create a map class

---
+++ lib/more_pages/maps.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SearchPlace extends StatefulWidget {
+  createState() {
+    return SearchPlaceState();
+  }
+}
+
+class SearchPlaceState extends State<SearchPlace> {
+  Widget build(context) {
+    return Scaffold();
+  }
+}
