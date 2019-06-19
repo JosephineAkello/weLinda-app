f527df50a5a71ae1552196dca41fc7d594d25e08
mj <onyangomaureen95@gmail.com>
2019-06-19 17:47:30 +0300

    re write arifu page

--- lib/arifu.dart
+++ lib/arifu.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'more_pages/counsellors.dart';
 import 'quickreach.dart';
+import 'more_pages/maps.dart';
 import 'faq.dart';
 
 class Arifu extends StatelessWidget {
@@ -62,14 +63,63 @@ class Arifu extends StatelessWidget {
                 )),
           ),
           Card(
-              child: Container(
-            padding: EdgeInsets.only(left: 16.0, right: 16.0),
-            width: MediaQuery.of(context).orientation == Orientation.portrait
-                ? 500
-                : 900,
-            height: 250.0,
-            child: QuickResearch(),
-          )),
+            child: Container(
+              padding: EdgeInsets.only(left: 16.0, right: 16.0),
+              width: MediaQuery.of(context).orientation == Orientation.portrait
+                  ? 500
+                  : 900,
+              height: 250.0,
+              child: Column(children: [
+                Text(
+                  'Quick Reach',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                    'You can search nearby recovery centers using the Google maps based on your location.'
+                    'There is  also a list of of Recovery centers that you can reach out to when you encounter '
+                    'Gender Based Violence attack'),
+                FlatButton(
+                    child: Text(
+                      'Find out More..',
+                      style: TextStyle(
+                        color: Colors.purple,
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => QuickResearch()));
+                    }),
+                Expanded(
+                  child: Row(children: [
+                    Text('Search nearest recovery center here!'),
+                    SizedBox(
+                      width: 20.0,
+                    ),
+                    IconButton(
+                      icon: Icon(
+                        Icons.location_searching,
+                        color: Colors.purple,
+                      ),
+                      onPressed: () { Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SearchPlace()));
+
+                      },
+                    ),
+                  ]),
+                ),
+              ]),
+            ),
+
+            // QuickResearch(),
+          ),
           Card(
               child: Container(
                   padding: EdgeInsets.only(left: 16.0, right: 16.0),
